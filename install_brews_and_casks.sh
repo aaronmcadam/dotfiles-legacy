f8edf77d088db42f581e5a891237d2b2ad7d6f57
Aaron Mc Adam <aaron@aaronmcadam.com>
2014-08-06 12:17:02 +0100

    Move Brewfile to a shell script as brew bundle isn't fully supported

---
+++ install_brews_and_casks.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+echo "================"
+echo "INSTALLING BREWS"
+echo "================"
+
+brew tap thoughtbot/formulae
+
+brew install ag
+brew install ctags
+brew install direnv
+brew install gh
+brew install git
+brew install git-flow
+brew install htop
+brew install mobile-shell
+brew install nginx
+brew install phantomjs
+brew install postgresql
+brew install pow
+brew install rcm
+brew install rename
+brew install tmux
+brew install tree
+brew install vim
+brew install zsh
+
+echo "================"
+echo "INSTALLING CASKS"
+echo "================"
+
+brew install caskroom/cask/brew-cask
+brew cask install alfred
+brew cask install bartender
+brew cask install bettertouchtool
+brew cask install day-o
+brew cask install dropbox
+brew cask install firefox
+brew cask install flowdock
+brew cask install fluid
+brew cask install google-chrome
+brew cask install jumpcut
+brew cask install ksdiff
+brew cask install licecap
+brew cask install sizeup
+brew cask install skitch
+brew cask install skype
+brew cask install virtualbox
+
+echo "==============================================="
+echo "ADDING CASKS DIRECTORY TO ALFRED'S SEARCH SCOPE"
+echo "==============================================="
+
+brew cask alfred link
