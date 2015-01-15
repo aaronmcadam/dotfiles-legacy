9897be6a1834e88833ecdeadd45218d4dbb95432
Aaron Mc Adam <aaron@aaronmcadam.com>
2015-01-15 12:05:35 +0000

    Adds rbenv-default-gems

--- install_brews_and_casks.sh
+++ install_brews_and_casks.sh
@@ -24,6 +24,7 @@ brew install pow
 brew install reattach-to-user-namespace
 brew install rbenv
 brew install rbenv-ctags
+brew install rbenv-default-gems
 brew install rcm
 brew install rename
 brew install ruby-build
